e08e6096fbce4bdd7ba80cb790660095262ec295
Gustavo De Sousa <gustavodesousa@gustavos-macbook-pro.local>
2018-10-14 00:06:43 -0300

    Initial Commit

---
+++ CampeoesDaCopa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CampeoesDaCopa
+//
+//  Created by Gustavo De Sousa on 14/10/18.
+//  Copyright © 2018 Gustavo De Sousa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
